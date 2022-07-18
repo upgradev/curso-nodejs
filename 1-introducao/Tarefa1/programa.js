2d361dc28cc1d2985719b47fed494ef282bed023
Cleison França Amorim <>
2022-07-18 15:21:37 -0400

    estudos

---
+++ 1-introducao/Tarefa1/programa.js
@@ -0,0 +1,4 @@
+var num1 = 2
+var num2 = 7
+var soma = num1 + num2
+console.log(soma);
---
+++ 2-Fundamentos-NodeJs/1-modulo_interno/index.js
@@ -0,0 +1,7 @@
+const meuModulo = require("./meu_modulo");
+const soma = meuModulo.soma;
+
+// meuModulo.soma(10, 10);
+
+soma(2, 3);
+soma(5, 10);
