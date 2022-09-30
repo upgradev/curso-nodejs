c1df16a56f3668e10cd8b58778ec368316158521
Cleison França Amorim <>
2022-09-30 14:05:52 -0400

    estudos

---
+++ 12_Node_MongoDB/8_editar_dado_salvar/db/conn.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+
+var uri = "mongodb://0.0.0.0:27017/testemongodb2";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Conectado ao mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run();
+
+module.exports = client;
